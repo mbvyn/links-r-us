232666219963044067f281b9be7a82c485ccfd0b
Mykola-Bohdan Vynnytskyi <qbodia@gmail.com>
2023-03-06 17:54:31 +0200

    Add fixed pool

--- pipeline/stage.go
+++ pipeline/stage.go
@@ -3,6 +3,7 @@ package pipeline
 import (
 	"context"
 	"golang.org/x/xerrors"
+	"sync"
 )
 
 type fifo struct {
@@ -41,6 +42,7 @@ func (r fifo) Run(ctx context.Context, params StageParams) {
 				continue
 			}
 
+			// Output processed data
 			select {
 			case params.Output() <- payloadOut:
 			case <-ctx.Done():
@@ -49,6 +51,42 @@ func (r fifo) Run(ctx context.Context, params StageParams) {
 		}
 
 	}
+}
+
+type fixedWorkerPool struct {
+	fifos []StageRunner
+}
+
+// FixedWorkerPool returns a StageRunner that spins up a pool containing
+// numWorkers to process incoming payloads in parallel and emit their outputs
+// to the next stage.
+func FixedWorkerPool(proc Processor, numWorkers int) StageRunner {
+	if numWorkers <= 0 {
+		panic("FixedWorkerPool: numWorkers must be > 0")
+	}
+
+	fifos := make([]StageRunner, numWorkers)
+	for i := 0; i < numWorkers; i++ {
+		fifos[i] = FIFO(proc)
+	}
+
+	return &fixedWorkerPool{fifos: fifos}
+}
+
+// Run implements StageRunner.
+func (p *fixedWorkerPool) Run(ctx context.Context, params StageParams) {
+	var wg sync.WaitGroup
+
+	// Spin up each worker in the pool and wait for them to exit
+	for i := 0; i < len(p.fifos); i++ {
+		wg.Add(1)
+		go func(fifoIndex int) {
+			p.fifos[fifoIndex].Run(ctx, params)
+			wg.Done()
+		}(i)
+	}
+	
+	wg.Wait()
 }
 
 // maybeEmitError attempts to queue err to a buffered error channel. If the
