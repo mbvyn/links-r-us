b7f1b92e75e4c276fee231bfdc6c469679b79d31
Mykola-Bohdan Vynnytskyi <qbodia@gmail.com>
2023-03-06 19:00:29 +0200

    Add Source interface

--- pipeline/interfaces.go
+++ pipeline/interfaces.go
@@ -64,3 +64,17 @@ type StageRunner interface {
 	// - an error occurs while processing payloads.
 	Run(context.Context, StageParams)
 }
+
+// Source is implemented by types that generate Payload instances which can be
+// used as inputs to a Pipeline instance.
+type Source interface {
+	// Next fetches the next payload from the source. If no more items are
+	// available or an error occurs, calls to Next return false.
+	Next(ctx context.Context) bool
+
+	// Payload returns the next payload to be processed.
+	Payload() Payload
+
+	// Error return the last error observed by the source.
+	Error() error
+}
