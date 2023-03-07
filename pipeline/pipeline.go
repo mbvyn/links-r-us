f5663c04f7b9f661d0d726e6f67c41bf55b93eeb
Mykola-Bohdan Vynnytskyi <qbodia@gmail.com>
2023-03-07 18:27:47 +0200

    Add sourceWorker function

--- pipeline/pipeline.go
+++ pipeline/pipeline.go
@@ -1,5 +1,10 @@
 package pipeline
 
+import (
+	"context"
+	"golang.org/x/xerrors"
+)
+
 var _ StageParams = (*workerParams)(nil)
 
 type workerParams struct {
@@ -15,3 +20,24 @@ func (p *workerParams) StageIndex() int        { return p.stage }
 func (p *workerParams) Input() <-chan Payload  { return p.inCh }
 func (p *workerParams) Output() chan<- Payload { return p.outCh }
 func (p *workerParams) Error() chan<- error    { return p.errCh }
+
+// sourceWorker implements a worker that reads Payload instances from a Source
+// and pushes them to an output channel that is used as input for the first
+// stage of the pipeline.
+func sourceWorker(ctx context.Context, source Source, outCh chan<- Payload, errCh chan<- error) {
+	for source.Next(ctx) {
+		payload := source.Payload()
+		select {
+		case outCh <- payload:
+		case <-ctx.Done():
+			// Asked to shutdown
+			return
+		}
+	}
+
+	// Check for errors
+	if err := source.Error(); err != nil {
+		wrappedErr := xerrors.Errorf("pipeline source: %w", err)
+		maybeEmitError(wrappedErr, errCh)
+	}
+}
