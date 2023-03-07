af085add878b5d122bc1f87fbe6f1513117e940e
Mykola-Bohdan Vynnytskyi <qbodia@gmail.com>
2023-03-07 18:18:11 +0200

    Add broadcast implementation

---
+++ pipeline/pipeline.go
@@ -0,0 +1,17 @@
+package pipeline
+
+var _ StageParams = (*workerParams)(nil)
+
+type workerParams struct {
+	stage int
+
+	// Channels for the worker's input, output and errors.
+	inCh  <-chan Payload
+	outCh chan<- Payload
+	errCh chan<- error
+}
+
+func (p *workerParams) StageIndex() int        { return p.stage }
+func (p *workerParams) Input() <-chan Payload  { return p.inCh }
+func (p *workerParams) Output() chan<- Payload { return p.outCh }
+func (p *workerParams) Error() chan<- error    { return p.errCh }
--- pipeline/stage.go
+++ pipeline/stage.go
@@ -161,6 +161,84 @@ stop:
 	}
 }
 
+type broadcast struct {
+	fifos []StageRunner
+}
+
+// Broadcast returns a StageRunner that passes a copy of each incoming payload
+// to all specified processors and emits their outputs to the next stage.
+func Broadcast(procs ...Processor) StageRunner {
+	if len(procs) == 0 {
+		panic("Broadcast: at least one processor must be specified")
+	}
+
+	fifos := make([]StageRunner, len(procs))
+	for i, p := range procs {
+		fifos[i] = FIFO(p)
+	}
+
+	return &broadcast{fifos: fifos}
+}
+
+// Run implements StageRunner.
+func (b *broadcast) Run(ctx context.Context, params StageParams) {
+	var (
+		wg   sync.WaitGroup
+		inCh = make([]chan Payload, len(b.fifos))
+	)
+
+	// Start each FIFO in a go-routine. Each FIFO gets its own dedicated
+	// input channel and the shared output channel passed to Run.
+	for i := 0; i < len(b.fifos); i++ {
+		wg.Add(1)
+		inCh[i] = make(chan Payload)
+		go func(fifoIndex int) {
+			fifoParams := &workerParams{
+				stage: params.StageIndex(),
+				inCh:  inCh[fifoIndex],
+				outCh: params.Output(),
+				errCh: params.Error(),
+			}
+			b.fifos[fifoIndex].Run(ctx, fifoParams)
+			wg.Done()
+		}(i)
+	}
+
+done:
+	for {
+		// Read incoming payloads and pass them to each FIFO
+		select {
+		case <-ctx.Done():
+			break done
+		case payload, ok := <-params.Input():
+			if !ok {
+				break done
+			}
+			for i := len(b.fifos) - 1; i >= 0; i-- {
+				// As each FIFO might modify the payload, to
+				// avoid data races we need to make a copy of
+				// the payload for all FIFOs except the first.
+				var fifoPayload = payload
+				if i != 0 {
+					fifoPayload = payload.Clone()
+				}
+				select {
+				case <-ctx.Done():
+					break done
+				case inCh[i] <- fifoPayload:
+					// payload sent to i_th FIFO
+				}
+			}
+		}
+	}
+
+	// Close input channels and wait for FIFOs to exit
+	for _, ch := range inCh {
+		close(ch)
+	}
+	wg.Wait()
+}
+
 // maybeEmitError attempts to queue err to a buffered error channel. If the
 // channel is full, the error is dropped.
 func maybeEmitError(err error, errCh chan<- error) {
